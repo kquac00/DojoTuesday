02e9e0bdeb17e5107a3665bc4fdc00b0affbdf37
kquac00 <kimlyq@gmail.com>
2023-03-28 18:58:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/LoginPage'
+import { Routes, Route, Link } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import Checkout from './components/Checkout'
+import ProductPage from './components/ProductPage'
+import Cart from './components/Cart'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> WELCOME TO DOJO COMMERCE </h1>
+      <p> Kimly - home page </p>
+      <p> Long - loginreg wrap up</p>
+      <p> Shreyas - github - then cart/checkout?</p>
+
+      <Link to="/test/product"> Product Page Test </Link>
+      <Link to="test/cart"> Cart Page Test </Link>
+      <Link to="/test/login"> Login Page Test </Link>
+      <Link to="test/checkout"> Checkout Page Test </Link>
+      <Link to="/"> Home Page Test </Link>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/test/cart" element={<Cart />} />
+        <Route path="/test/checkout" element={<Checkout />} />
+        <Route path="/test/login" element={<LoginPage />} />
+        <Route path="/test/product" element={<ProductPage />} />
+      </Routes>
     </div>
   );
 }
